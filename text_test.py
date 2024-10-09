b3fe31839ecae0758ec9272f08d1f2ce7576a1f1
LOKESH YARRAMALLU <lokeshyarramalluyarramalluloke@gmail.com>
2024-10-09 17:25:04 +0530

    required

--- text_test.py
+++
@@ -1,9 +0,0 @@
-from text import QueryAnswering
-
-def test_query_answering():
-    qa = QueryAnswering()
-    response = qa.get_query_answering("What is the capital of France?")
-    print(response)  # Just print the response for manual verification
-
-if __name__ == "__main__":
-    test_query_answering()
