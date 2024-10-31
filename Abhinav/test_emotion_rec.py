13a7dfdb8a6299e191028df15015547c474a1fb0
AbhinavRudra <AbhinavRudra>
2024-10-31 18:42:33 +0530

    sound_emotion_recognition

---
+++ Abhinav/test_emotion_rec.py
@@ -0,0 +1,6 @@
+# Example usage:
+from Abhinav.speechEmotion import EmotionRecognition
+
+att = EmotionRecognition("Your-API-Key")
+output = att.EmotionRecognition("Abhinav/emotional.wav")
+print(output)
